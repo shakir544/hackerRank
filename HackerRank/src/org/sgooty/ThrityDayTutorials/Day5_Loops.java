48e2fd7df411dd475de098ac3880718d32701510
Shakir Gooty <shakir544@gmail.com>
2016-09-08 19:44:17 -0400

    30 Days of coding - Day 5 - Loops

---
+++ HackerRank/src/org/sgooty/ThrityDayTutorials/Day5_Loops.java
@@ -0,0 +1,17 @@
+package org.sgooty.ThrityDayTutorials;
+
+
+import java.util.*;
+
+public class Day5_Loops {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        
+        for(int i =1; i <=10 ;i++) { 
+            System.out.println(N + " x " + i +" = "+ N*i );
+        }
+    }
+}
+
