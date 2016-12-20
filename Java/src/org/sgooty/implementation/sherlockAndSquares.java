bd8efc3aa186e43fcc74c3856a9b8b232d1eb483
Shakir Gooty <shakir544@gmail.com>
2016-12-19 16:02:02 -0800

    Renaming the name of the folder to have proper granularity

---
+++ Java/src/org/sgooty/implementation/sherlockAndSquares.java
@@ -0,0 +1,20 @@
+package org.sgooty.implementation;
+
+import java.util.Scanner;
+
+public class sherlockAndSquares {
+	
+	public static void main(String args[]){ 
+		
+		Scanner input = new Scanner(System.in); 
+		int T = input.nextInt(); 
+		
+		for(int i=0; i <T; i++){ 
+			String values[] = input.nextLine().split(" ");
+			for(int len =0; len <values.length;len++){ 
+				
+			}
+		}
+	}
+
+}
---
+++ Java/src/org/sgooty/implementation/sherlockTheBeast.java
@@ -0,0 +1,33 @@
+package org.sgooty.implementation;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class sherlockTheBeast {
+	
+	/*
+	 * Sherlock Holmes suspects his archenemy, Professor Moriarty, is once again plotting something diabolical. 
+	 * Sherlock's companion, Dr. Watson, suggests Moriarty may be responsible for MI6's recent issues with their supercomputer, The Beast.
+	   Shortly after resolving to investigate, Sherlock receives a note from Moriarty boasting about infecting The Beast with a virus; however, he also gives him a clue—a number, NN. Sherlock determines the key to removing the virus is to find the largest Decent Number having NN digits.
+	   A Decent Number has the following properties:
+	   Its digits can only be 3's and/or 5's.
+	   The number of 3's it contains is divisible by 5.
+	   The number of 5's it contains is divisible by 3.
+	   If there are more than one such number, we pick the largest one.
+	   Moriarty's virus shows a clock counting down to The Beast's destruction, and time is running out fast. 
+	   Your task is to help Sherlock find the key before The Beast is destroyed!
+	 */
+	
+	public static void main(String args[]) { 
+		Scanner in = new Scanner(System.in); 
+		int T = in.nextInt(); // T- number of test cases 
+		
+		int[] output = new int[T];
+		for(int i=0;i<T;i++){ 
+			int n = in.nextInt(); // n - number of digits in the number 
+			
+		}
+		
+	}
+
+}
