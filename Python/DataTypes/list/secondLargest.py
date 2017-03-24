ed47fc97c5dde90c1292b107f6e11ec9ecbf3eab
Shakir Gooty <shakir544@gmail.com>
2017-03-24 00:02:56 -0400

    Find the second largest number

---
+++ Python/DataTypes/list/secondLargest.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int,input().split()))
+    # Get the max out of the list
+    max_i = max(arr)
+    filter_arr = list(filter(lambda x: x!=max_i, arr))
+    print(max(filter_arr))
+
+
+
+    
+
+
+
+
